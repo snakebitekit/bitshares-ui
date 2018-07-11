3204744e27034c5748d21750254339549f60dda8
snakebitekit <vasiliyandreich@yandex.ru>
2018-07-11 20:48:52 +0300

    chart color fix

--- app/assets/colors.js
+++ app/assets/colors.js
@@ -7,8 +7,8 @@ module.exports = {
         callColor: "#BBBF2B",
         settleColor: "rgba(125, 134, 214, 1)",
         settleFillColor: "rgba(125, 134, 214, 0.5)",
-        positiveColor: "#00FF02",
-        negativeColor: "#DB0001",
+        positiveColor: "#6BA583",
+        negativeColor: "#DB0000",
         strokeColor: "#FFFF00",
         primaryText: "#e0e0e0",
         volumeColor: "#848484",
@@ -19,31 +19,36 @@ module.exports = {
         tooltipFillColor: "#fff",
         //axis
         axisLabelsColor: "#fff",
-        axisLineColor: "#AAAAAA",
-        indicatorLineColor: "#FFFFFF"
+        axisLineColor: "rgba(200, 200, 200, 0.3)",
+        indicatorLineColor: "#FFFFFF",
+        bgColor: "#191a1f",
+        textColor: "#e0e0e0"
     },
     darkTheme: {
-        bidColor: "#00FF02",
-        bidFillColor: "rgba(0, 255, 2, 0.3)",
-        askColor: "#DB0001",
-        askFillColor: "rgba(219, 0, 1, 0.3)",
-        callColor: "#BBBF2B",
-        settleColor: "rgba(125, 134, 214, 1)",
-        settleFillColor: "rgba(125, 134, 214, 0.5)",
-        positiveColor: "#258A14",
-        negativeColor: "#DB0001",
+        bidColor: "rgb(0, 255, 2)",
+        bidFillColor: "rgb(0, 255, 2)",
+        askColor: "rgb(219, 0, 1)",
+        askFillColor: "rgb(219, 0, 1)",
+        callColor: "rgb(219, 0, 1)",
+        settleColor: "rgb(125, 134, 2)",
+        settleFillColor: "rgb(125, 134, 214)",
+        positiveColor: "rgb(37, 138, 20)",
+        negativeColor: "rgb(219, 0, 1)",
         strokeColor: "#FFFF00",
         primaryText: "#e0e0e0",
         volumeColor: "#848484",
 
         //tooltip
-        tooltipBackgroundColor: "rgba(0, 0, 0, 0.4)",
+        tooltipBackgroundColor: "rgb(219, 0, 1)",
         tooltipColor: "#fff",
         tooltipFillColor: "#fff",
         //axis
         axisLabelsColor: "#fff",
         axisLineColor: "#AAAAAA",
-        indicatorLineColor: "#FFFFFF"
+        indicatorLineColor: "#FFFFFF",
+        indicatorLineColor: "#FFFFFF",
+        bgColor: "#000000",
+        textColor: "#ffffff"
     },
     lightTheme: {
         bidColor: "#258A14",
@@ -65,7 +70,9 @@ module.exports = {
         tooltipFillColor: "#000",
         //axis
         axisLabelsColor: "#000",
-        axisLineColor: "#000",
-        indicatorLineColor: "#848484"
+        axisLineColor: "rgba(200, 200, 200, 0.3)",
+        indicatorLineColor: "#848484",
+        bgColor: "#fff",
+        textColor: "#3d3d3d"
     }
 };
